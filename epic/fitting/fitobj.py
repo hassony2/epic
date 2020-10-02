ac49aeaff9da0692229bb47d17447c3b6e8e675a
Yana <yanahasson@gmail.com>
2020-10-02 14:31:59 +0200

    Add precommit info

--- epic/fitting/fitobj.py
+++ epic/fitting/fitobj.py
@@ -2,10 +2,10 @@ from collections import defaultdict
 import os
 from pathlib import Path
 
-import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 from pytorch3d.io import load_obj as py3dload_obj
+from scipy.stats import special_ortho_group 
 import torch
 from tqdm import tqdm
 
@@ -21,6 +21,22 @@ os.environ["FFMPEG_BINARY"] = "/sequoia/data3/yhasson/miniconda3/bin/ffmpeg"
 import moviepy.editor as mpy
 
 
+def repeatdim(tens, repeat_nb, undim=0):
+    """
+    Creates copies of tensor 
+    """
+    if repeat_nb == 1:
+        flat_tens = tens
+    else:
+        assert undim in [0, 1], f"undim {undim} not in [0,1]"
+        or_shape = tens.shape
+        repeat_vals = [1 for _ in range(tens.dim() + 1)]
+        repeat_vals[undim] = repeat_nb
+        rep_tens = tens.unsqueeze(undim).repeat(*repeat_vals)
+        flat_tens = rep_tens.view(or_shape[0] * repeat_nb, *or_shape[1:])
+    return flat_tens
+
+
 def fitobj2mask(
     masks,
     bboxes,
@@ -33,9 +49,10 @@ def fitobj2mask(
     iters=100,
     viz_step=1,
     save_folder="tmp/",
-    viz_rows=4,
+    viz_rows=12,
     crop_box=True,
     crop_size=(200, 200),
+    rot_nb=1,
 ):
     # Initialize logging info
     opts = {
@@ -86,12 +103,22 @@ def fitobj2mask(
             num_dims=height * width, float_dtype=np.float32, device="cuda:0"
         )
     # Prepare rigid parameters
-    rot_vec = torch.Tensor([[1, 0, 0, 0, 1, 0] for _ in range(batch_size)]).cuda()
+    if rot_nb > 1:
+        rot_mats = [special_ortho_group.rvs(3) for _ in range(rot_nb)]
+        rot_vecs = torch.Tensor([np.linalg.svd(rot_mat)[0][:2].reshape(-1) for rot_mat in rot_mats])
+        rot_vec = rot_vecs.repeat(batch_size, 1) .cuda()
+        # Ordering b1 rot1, b1 rot2, ..., b2 rot1, ...
+    else:
+        rot_vec = torch.Tensor([[1, 0, 0, 0, 1, 0] for _ in range(batch_size)]).cuda()
+
     bboxes = torch.stack(bboxes)
-    trans = ops3d.trans_init_from_boxes(bboxes, camintr, (z_off, z_off))
+    trans = ops3d.trans_init_from_boxes(bboxes, camintr, (z_off, z_off)).cuda()
+    # Repeat to match rots
+    trans = repeatdim(trans, rot_nb, 1)
     bboxes = boxutils.pad(bboxes)
     if crop_box:
         camintr_crop = camutils.get_K_crop_resize(camintr, bboxes, crop_size)
+    camintr_crop = repeatdim(camintr_crop, rot_nb, 1)
 
     trans.requires_grad = True
     rot_vec.requires_grad = True
@@ -103,6 +130,7 @@ def fitobj2mask(
     ref_masks = torch.stack(masks).cuda()
     if crop_box:
         ref_masks = cropping.crops(ref_masks.float(), bboxes, crop_size)[:, 0]
+
     # Prepare reference mask
     if "dtf" in loss_type:
         target_masks = torch.stack(
@@ -110,6 +138,10 @@ def fitobj2mask(
         ).cuda()
     else:
         target_masks = ref_masks
+    ref_masks = repeatdim(ref_masks, rot_nb, 1)
+    target_masks = repeatdim(target_masks, rot_nb, 1)
+    batch_verts = repeatdim(batch_verts, rot_nb, 1)
+    batch_faces = repeatdim(batch_faces, rot_nb, 1)
 
     col_nb = 5
     fig_res = 1.5
@@ -151,12 +183,12 @@ def fitobj2mask(
         elif "l1" in loss_type:
             loss = optim_mask_diff.abs().mean()
         elif "adapt" in loss_type:
-            loss = adaptive_loss.lossfun(optim_mask_diff.view(batch_size, -1)).mean()
+            loss = adaptive_loss.lossfun(optim_mask_diff.view(rot_nb * batch_size, -1)).mean()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         if iter_idx % viz_step == 0:
-            row_idxs = np.linspace(0, batch_size - 1, viz_rows).astype(np.int)
+            row_idxs = np.linspace(0, batch_size * rot_nb - 1, viz_rows).astype(np.int)
             row_nb = viz_rows
             fig, axes = plt.subplots(
                 row_nb, col_nb, figsize=(int(col_nb * fig_res), int(row_nb * fig_res))
